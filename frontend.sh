38aaae620dca15c5615fca8aa929794b9503bdbe
neeli <neeliveti.kirankumar@gmai.com>
2023-07-22 21:43:12 +0530

    created frontend

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,6 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
 #need to configure vim here
 systemctl restart nginx
 systemctl enable nginx
