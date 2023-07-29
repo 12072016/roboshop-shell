a4c1e271f967afb46c27c76eea0c805db519e353
neeli <neeliveti.kirankumar@gmai.com>
2023-07-29 22:37:56 +0530

    Mysql script

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,15 @@
+
+echo -e "\e[36m>>>>>>>disable my sql default 8 version<<<<<<<<\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>coping my sql repo file<<<<<<<<\e[0m"
+
+cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>install my sql<<<<<<<<\e[0m"
 yum install mysql-community-server -y
 
-systemctl enable mysqld
-systemctl start mysqld
+echo -e "\e[36m>>>>>>>starting my sql <<<<<<<<\e[0m"
 
+systemctl enable mysqld
+systemctl restart mysqld
+echo -e "\e[36m>>>>>>>reset my sql password<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
