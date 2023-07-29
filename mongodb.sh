9f592a9b6851608543565b1aa7e859e9e774a901
neeli <neeliveti.kirankumar@gmai.com>
2023-07-29 16:49:48 +0530

    changed catalouge restart option

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,8 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
+sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
 systemctl enable mongod
 systemctl start mongod
 
-### Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
