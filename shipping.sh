a268552cb80d757cb2aeb2e24443d843164c92d6
neeli <neeliveti.kirankumar@gmai.com>
2023-07-30 14:27:17 +0530

    shipping script

--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,34 @@
+echo -e "\e[34m>>>>>>>install maven repo<<<<<<<<\e[0m"
+
 yum install maven -y
+echo -e "\e[34m>>>>>>>creat app user<<<<<<<<\e[0m"
+
 useradd roboshop
-useradd roboshop
+echo -e "\e[34m>>>>>>>icreate app directory<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[34m>>>>>>>download app content<<<<<<<<\e[0m"
+
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+echo -e "\e[34m>>>>>>>unzip app content<<<<<<<<\e[0m"
+
 cd /app
 unzip /tmp/shipping.zip
-cd /app
+
+echo -e "\e[34m>>>>>>>download maven dependencies<<<<<<<<\e[0m"
+
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+
+echo -e "\e[34m>>>>>>>install mysql <<<<<<<<\e[0m"
+yum install mysql -y
+echo -e "\e[34m>>>>>>>load schema<<<<<<<\e[0m"
+mysql -h <mysql-dev.kirandevops.online> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+
+echo -e "\e[34m>>>>>>>start shipping service<<<<<<<<\e[0m"
 systemctl daemon-reload
 
 systemctl enable shipping
-systemctl start shipping
-yum install mysql -y
-mysql -h <mysql-dev.kirandevops.online> -uroot -pRoboShop@1 < /app/schema/shipping.sql
 systemctl restart shipping
