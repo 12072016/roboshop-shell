4ff67eaef632afbe26446aaba8a5e6fc90783fe7
neeli <neeliveti.kirankumar@gmai.com>
2023-08-04 16:28:43 +0530

    added script_path

--- cart.sh
+++ cart.sh
@@ -8,33 +8,4 @@ source ${script_path}/common.sh
 component=cart
 func_nodejs
 
-echo -e "\e[36m>>>>>>>configuring nodejs repo<<<<<<<<\e[0m"
-
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[36m>>>>>>>install nodejs repo<<<<<<<<\e[0m"
-yum install nodejs -y
-echo -e "\e[36m>>>>>>>adding app user<<<<<<<<\e[0m"
-useradd ${app_user}
-echo -e "\e[36m>>>>>>>creating app directory <<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-echo -e "\e[36m>>>>>>>downloading app content<<<<<<<<\e[0m"
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-
-cd /app
-echo -e "\e[36m>>>>>>>unzip app content<<<<<<<<\e[0m"
-unzip /tmp/cart.zip
-
-echo -e "\e[36m>>>>>>>downloading app dependencies<<<<<<<<\e[0m"
-npm install
-echo -e "\e[36m>>>>>>>creating app directory<<<<<<<<\e[0m"
-cp ${script_path}/cart.service /etc/systemd/system/cart.service
-
-echo -e "\e[36m>>>>>>>starting cart service<<<<<<<<\e[0m"
-
-systemctl daemon-reload
-
-
-systemctl enable cart
-systemctl restart cart
 
